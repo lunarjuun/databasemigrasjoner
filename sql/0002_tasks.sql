03e10a18330e339ad95f4c074ddc1f24b06cab08
Dom <157792207+lunarjuun@users.noreply.github.com>
2026-04-14 11:07:36 +0200

    Update 0002_tasks.sql

--- sql/0002_tasks.sql
+++ sql/0002_tasks.sql
@@ -1,13 +1,25 @@
-CREATE TABLE (
+CREATE TABLE tasks (
     id SERIAL,
     title VARCHAR(255) NOT NULL,
     description TEXT,
     status VARCHAR(20) NOT NULL,
     assignee_id INTEGER,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    completed at TIMESTAMPTZ,
+    completed_at TIMESTAMPTZ,
 
     FOREIGN KEY (assignee_id)
-    REFERENCES users(id)
-    ON DELETE SET NULL
-)
+        REFERENCES users(id)
+        ON DELETE SET NULL,
+
+    CHECK (status IN (
+        'BACKLOG',
+        'READY',
+        'IN_PROGRESS',
+        'IN_REVIEW',
+        'IN_TESTING'
+        'COMPLETED'
+    ))
+
+    CREATE INDEX idx_tasks_assignee_id ON tasks(assignee_id);
+    CREATE INDEX idx_tasks_status ON tasks(status);
+);
